14c34fbabddfa7e89971e7110908dd13631685e2
newievong <143970329+newievong@users.noreply.github.com>
2023-11-02 19:17:11 -0400

    Update circlechallenge.py

--- circlechallenge.py
+++ circlechallenge.py
@@ -5,34 +5,43 @@ class Circle:
 
 
    def calculate_diameter(self):
-       diameter = self.radius * 2
+       diameter = self.radius * 2.0
        return f'Diameter: {diameter}'
+
    def calculate_circumference(self):
-       circumference = self.radius * 2 * math.pi
+       circumference = self.radius * 2.0 * math.pi
        return f'Circumference: {circumference}'
 
 
    def calculate_area(self):
-       area = math.pi * self.radius ** 2
+       area = math.pi * (self.radius ** 2.0)
        return f'Area: {area}'
 
 
    def grow(self):
-       self.radius = self.radius * 2
+       self.radius = self.radius * 2.0
 
 
    def get_radius(self):
        return self.radius
 
-
+def value_type(a):
+    try:
+        value = float(a)
+        return 'float'
+    except ValueError:
+        return 'str'
 
 
 while True:
-    radius = float(input("Welcome to the Circle Tester. Enter a radius up to two decimal points: "))
+    radius = input("Welcome to the Circle Tester. Enter a radius up to two decimal points: ")
 
-    while radius < 0:
-	    radius = int(input('That is not a valid value. Please enter a positive number, up to two decimal points: '))
-
+    while value_type(radius) == 'str':
+        print('Please select a number.')
+        radius = input("Welcome to the Circle Tester. Enter a radius up to two decimal points: ")
+    else:
+        radius = float(radius)
+        print("Calculating...")
 
     circle = Circle(radius)
     print(circle.calculate_diameter())
@@ -54,6 +63,7 @@ while True:
     else:
         print("This is not a valid input. Please try again.")
         break
+
 
 
 
